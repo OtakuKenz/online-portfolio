c1b7b43df498eb644fcc5e203d9efc780a9af6b6
otakukenz <otakukenz@icloud.com>
2025-04-12 01:10:09 +0800

    removed card and added added sign for collapse/expand

--- src/app/experience-and-education/experience-and-education.component.ts
+++ src/app/experience-and-education/experience-and-education.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { PriorToDevelopmentComponent } from "./prior-to-development/prior-to-development.component";
 import { AsDeveloperComponent } from "./as-developer/as-developer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experience-and-education',
-  imports: [PriorToDevelopmentComponent, AsDeveloperComponent],
+  imports: [CommonModule, PriorToDevelopmentComponent, AsDeveloperComponent],
   templateUrl: './experience-and-education.component.html',
   styleUrl: './experience-and-education.component.scss'
 })
 export class ExperienceAndEducationComponent {
+  expandedSections: { [key: string]: boolean } = {
+    priorToDevelopment: true,
+    asDeveloper: true
+  };
 
+  isExpanded(sectionId: string): boolean {
+    return this.expandedSections[sectionId];
+  }
+
+  toggleSection(sectionId: string): void {
+    this.expandedSections[sectionId] = !this.expandedSections[sectionId];
+  }
 }
